4bec5fd6c5b78256502033f2d3a6d49596b34fec
Jakub Górecki <jakub.xiz1.gorecki@gmail.com>
2022-05-31 23:28:07 +0200

    ADD TicTacToe class files.

---
+++ inc/TicTacToe.h
@@ -0,0 +1,12 @@
+/*
+* Copyright (C) 2022 Jakub Górecki - All Right Reserved.
+* Gmail: jakub.xiz1.gorecki@gmail.com
+* GitHub: https://github.com/XiZ1
+*/
+
+#pragma once
+
+class TicTacToe
+{
+
+};
---
+++ src/TicTacToe.cpp
@@ -0,0 +1,7 @@
+/*
+* Copyright (C) 2022 Jakub Górecki - All Right Reserved.
+* Gmail: jakub.xiz1.gorecki@gmail.com
+* GitHub: https://github.com/XiZ1
+*/
+
+#include "TicTacToe.h"

f5cd2fd2eec5cd515524655441a3a1c1aacca37c
Jakub <jakub.xiz1.gorecki@gmail.com>
2022-05-31 22:21:13 +0100

    Dodaj pliki projektów.

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+* Copyright (C) 2022 Jakub Górecki - All Right Reserved.
+* Gmail: jakub.xiz1.gorecki@gmail.com
+* GitHub: https://github.com/XiZ1
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!\n";
+
+	return 0;
+}
